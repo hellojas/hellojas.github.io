7ed3e74f0a71e8b2ccf404c021640b1e61257a36
Jasmine <jasminechsu@gmail.com>
2025-08-21 15:16:39 -0400

    Update log.js

--- office/log.js
+++ office/log.js
@@ -1,23 +1,17 @@
-// Debug logging
-console.log('log.js loading...');
-
-// Firebase configuration (replace with your config)
+// Firebase configuration
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js';
 import { getDatabase, ref, set, serverTimestamp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js';
 
-// Your Firebase config object (replace with your actual config)
 const firebaseConfig = {
   apiKey: "AIzaSyCJIAxjywQhuKuqWqBa4FgUZuM6RrQ7y-E",
   authDomain: "office-127b0.firebaseapp.com",
-  databaseURL: "https://office-127b0-default-rtdb.firebaseio.com/", // ADD THIS LINE
+  databaseURL: "https://office-127b0-default-rtdb.firebaseio.com/",
   projectId: "office-127b0",
   storageBucket: "office-127b0.firebasestorage.app",
   messagingSenderId: "554800223425",
   appId: "1:554800223425:web:ecb87d46fee7c3e5a1f040",
   measurementId: "G-8WXVRR5VJD"
 };
-
-console.log('Initializing Firebase...');
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
